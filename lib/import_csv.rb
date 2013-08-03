e8f5748853b4853ce27fdcb2ecb742d30967c61f
Hart <HartSimha@gmail.com>
2013-08-02 20:43:27 -0700

    Update import_csv.rb
    added import_skyward_students method
    
    fixed what I think were a couple minor bugs
    
    also added some comments for my benefit that aren't intended to be merged back with the original repository

--- lib/import_csv.rb
+++ lib/import_csv.rb
@@ -1,11 +1,11 @@
 class ImportCSV
   require File.expand_path 'lib/csv_importer/base_system_flags.rb'
 
-  APPEND_FILE_MATCHER = /_append(s)?/
+  APPEND_FILE_MATCHER = /_appends?/
   DELETE_COUNT_THRESHOLD = 5
   DELETE_PERCENT_THRESHOLD = 0.3
-  STRIP_FILTER = lambda{ |field| field.to_s.strip}
-  NULLIFY_FILTER = lambda{ |field| field == "NULL" ? nil : field}
+  STRIP_FILTER = lambda{ |field| field.to_s.strip} #formats as string with leading and trailing whitespace removed
+  NULLIFY_FILTER = lambda{ |field| field == "NULL" ? nil : field} #returns nil if field == "NULL". Otherwise, returns field
   HEXIFY_FILTER  = lambda{ |field| hex=field.to_i(16).to_s(16); hex.length == 40 ? hex : field}
   CLEAN_CSV_OPTS ={:converters => [STRIP_FILTER,:symbol]}
   DEFAULT_CSV_OPTS={:skip_blanks=>true, :headers =>true, :header_converters => [STRIP_FILTER,:symbol], :converters => [STRIP_FILTER,NULLIFY_FILTER,HEXIFY_FILTER]}
@@ -18,17 +18,17 @@ class ImportCSV
     "all_schools.csv", "all_students_in_district.csv","all_students_in_school.csv", "user_school_assignments.csv", "staff_assignments.csv",
     "ext_arbitraries.csv", "ext_siblings.csv", "ext_adult_contacts.csv", "ext_test_scores.csv", "ext_summaries.csv",
     "district_admins.csv","news_admins.csv", "content_admins.csv", "school_admins.csv", "regular_users.csv", "system_flags.csv",
-    "admins_of_schools.csv",
+    "admins_of_schools.csv", "skyward_students.csv",
     *Flag::FLAGTYPES.keys.collect{|e| "#{e}_system_flags.csv"}
     ]
 
-  def self.importers
+  def self.importers #array mapping from VALID_FILES where element 'all_students_in_school' => CSVImporter::AllStudentsInSchools
     VALID_FILES.sort.collect do |csv_file|
       "CSVImporter::#{csv_file.split('.').first.classify.pluralize}".constantize
     end
   end
 
-  def self.importer_from_symbol(key)
+  def self.importer_from_symbol(key) #e.g. :test_sym => CSVImporter::TestSyms
    "CSVImporter::#{key.to_s.classify.pluralize}".constantize
   end
 
@@ -46,6 +46,7 @@ class ImportCSV
   def import
     b= Benchmark.measure do
       identify_and_unzip
+      process_skyward_students if @filenames.include? File.join(@f_path, "skyward_students.csv")
       sorted_filenames.each {|f| process_file f}
       FileUtils.rm_rf @f_path
       @district.students.update_all(:updated_at => Time.now) #expire any student related cache
@@ -58,8 +59,19 @@ class ImportCSV
   end
 
   private
+
   def update_memcache
     Rails.cache.write("#{@district.id}_import", @messages.join("<br/ > "), expires_in: 120.minutes)
+  end
+
+  def process_skyward_students
+    unless @filenames.include? File.join(@f_path, "enrollments.csv") or @filenames.include? File.join(@f_path, "students.csv")      
+      system ">#{File.join(@f_path,'enrollments.csv')}"
+      system ">#{File.join(@f_path,'students.csv')}"
+      @filenames << File.join(@f_path,'enrollments.csv') << File.join(@f_path,'students.csv')
+    else
+      @messages << "You cannot have enrollments or students.csv when skyward_students.csv is present"
+      update_memcache
   end
 
   def process_file file_name
@@ -71,7 +83,7 @@ class ImportCSV
     when *csv_importers
       csv_importer file_name
     else
-      msg = "Unknown file #{base_file_name}"
+      msg = "Unknown file #{base_file_name}" #how is this not causing an error?
     end
 
     @messages << msg
@@ -83,8 +95,8 @@ class ImportCSV
   end
 
   def csv_importer file_name
-    base_file_name = File.basename(file_name).gsub(APPEND_FILE_MATCHER,'')
-    c="CSVImporter/#{base_file_name.sub(/.csv/,'')}".classify.pluralize
+    base_file_name = File.basename(file_name).gsub(APPEND_FILE_MATCHER,'') #"/path/to/file/students_appendix.csv" => "studentsix.csv"
+    c="CSVImporter/#{base_file_name.sub(/.csv/,'')}".classify.pluralize #"studentsix.csv" => "CSVImporter::Studentsixes"
     @messages << c.constantize.new(file_name,@district).import
   end
 
@@ -99,7 +111,6 @@ class ImportCSV
   end
 
   def sorted_filenames filenames=@filenames
-
     filenames.compact.sort_by do |f|
       2 * (FILE_ORDER.index(File.basename(f.downcase.gsub(APPEND_FILE_MATCHER,'')))  || FILE_ORDER.length) +
       (f.match(APPEND_FILE_MATCHER) ? 1 : 0)
@@ -109,13 +120,13 @@ class ImportCSV
   def identify_and_unzip
     FileUtils.mkdir_p(@f_path)
     if @file.respond_to?(:original_filename)
-      try_to_unzip(@file.path, @file.original_filename) or move_to_import_directory
+      try_to_unzip(@file.path, @file.original_filename) or move_to_import_directory if VALID_FILES.include? File.basename(@file.original_filename)
     else  #passed in a string
-      try_to_unzip(@file, @file) or @filenames =[@file]
+      try_to_unzip(@file, @file) or move_to_import_directory File.new(@file) if VALID_FILES.include? File.basename(@file) unless Dir.glob(@file).empty? #need to test this
     end
   end
 
-  def move_to_import_directory
+  def move_to_import_directory file=@file
     base_filename = File.basename(@file.original_filename)
     new_filename= File.join(@f_path,base_filename)
     FileUtils.mv @file.path,new_filename
@@ -128,13 +139,17 @@ class ImportCSV
   #file zip
 
 
-  def try_to_unzip filename, originalname
+  def try_to_unzip filename, originalname #unzips file to temporary directory and stores contained .csv filenames in @filenames
     if originalname =~ /\.zip$/
       @messages << "Trying to unzip #{originalname}"
       update_memcache
-
+      #potential replacement for next two lines follows
       @messages << "Problem with zipfile #{originalname}" unless
         system "unzip  -qq -o #{filename} -d #{@f_path}"
+      #unless system "unzip  -qq -o #{filename} -d #{@f_path}"
+        #@messages << "Problem with zipfile #{originalname}"
+        #update_memcache
+      #end
       @filenames = Dir.glob(File.join(@f_path, "*.csv"))
     else
       false
